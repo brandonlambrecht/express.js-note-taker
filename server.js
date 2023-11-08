c7fc36359c8a73c6cbfadf5ee9205c96a8bc26f8
brandonlambrecht <brandonjlambrecht@gmail.com>
2023-11-07 22:00:40 -0600

    pushing to heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', noteRouter);
-console.log('I got here')
+
 
 app.use(express.static('public'));
 
